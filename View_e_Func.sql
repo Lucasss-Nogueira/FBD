7bded6629201c1fe009a4c6e6c6ab9802ce2c4a8
Arthur Pinto Bezerra <93888011+Arthurpbezerra@users.noreply.github.com>
2022-12-10 18:52:10 -0300

    Update View_e_Func.sql
    add 8c

--- View_e_Func.sql
+++ View_e_Func.sql
@@ -1,7 +1,3 @@
---CODIGOS SEM TESTES
-
-
-
 create view ver_playlist(nome_playlist, qtde_album)
 with
   schemabinding
@@ -22,7 +18,21 @@ returns @tab_result(@nome_alb varchar(40))
 as
 begin
 insert into @tab_result
-select alb.nome from Album alb inner join Faixa f inner join Faixa_Compositor fc inner join Compositor cp on alb.cod_alb = f.cod_alb on f.num_faixa = fc.num_faixa on fc.cod_compositor =  cp.cod_compositor
+select distinct alb.nome from Album alb inner join Faixa f inner join Faixa_Compositor fc inner join Compositor cp on alb.cod_alb = f.cod_alb on f.num_faixa = fc.num_faixa on fc.cod_compositor =  cp.cod_compositor
 where cp.nome = @nome_cp
 return
 end
+
+
+
+-----
+--8c
+--Listar nome do compositor com maior número de faixas nas playlists existentes
+select cp.nome from Compositor cp left outer join Faixa_Compositor fc left outer join Faixa f left outer join Faixa_Playlist fp left outer join Playlist p 
+on fc.cod_compositor =  cp.cod_compositor on f.num_faixa = fc.num_faixa on fp.num_faixa = f.num_faixa on p.cod_playlist = fp.cod_playlist
+group by cp.cod_compositor, cp.nome
+having count(distinct fc.num_faixa) >= all select (count(distinct f.num_faixa) from Faixa f, Faixa_Playlist fp, Playlist p 
+where f.num_faixa = fp.num_faixa and fp.cod_playlist = p.cod_playlist group by f.num_faixa)
+
+
+--8d
