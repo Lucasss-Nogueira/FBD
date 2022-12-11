4f518399906a6238d673d676436aa44844f2f1ed
Lucasss-Nogueira <104247878+Lucasss-Nogueira@users.noreply.github.com>
2022-12-11 03:02:54 -0300

    Iniciando o app

---
+++ Consultas para o app.sql
@@ -0,0 +1,4 @@
+select a.cod_album, f.num_faixa, f.descri
+	from album a inner join faixa f on a.cod_album=f.cod_album
+
+insert into album values (1,'cartao','um album mt massa',123,432,'11/02/2222','11/02/2223','vinil',1)
